7326292eff8e9a66d60b8c2aea59ed5d162ed624
@picocreator (Eugene Cheah) <eugeneqin@gmail.com>
2024-02-24 17:00:23 -0800

    reduce the wget verbosity

--- gh-task-runner/prepare-rwkv-v5-pth.sh
+++ gh-task-runner/prepare-rwkv-v5-pth.sh
@@ -35,7 +35,7 @@ fi
 # Download the model
 echo "### Downloading the model from $RWKV_PTH_URL"
 rm "$PROJ_DIR/model/rwkv-v5.pth"
-wget -O "$PROJ_DIR/model/rwkv-v5.pth" "$RWKV_PTH_URL"
+wget -nv -O "$PROJ_DIR/model/rwkv-v5.pth" "$RWKV_PTH_URL"
 echo "### Downloaded the model"
 
 # Get the file size
